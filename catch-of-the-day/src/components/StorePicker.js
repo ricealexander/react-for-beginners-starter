3cea8b03b3afebf010dc075847daa9784d14a085
ricealexander <webdev@ricealexander.com>
2019-07-06 09:04:46 -0500

    :one::one: learn about binding functions in React

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  myInput = React.createRef();
+
   goToStore(event) {
     // 1. Stop the form from submitting
     event.preventDefault();
     // 2. get the text from that input
+    console.log(this);
     // 3. Change the page to /store/whatever-they-entered
   }
+
   render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input
           type="text"
+          ref="this.myInput"
           placeholder="Store Name"
           defaultValue={getFunName()}
           required
