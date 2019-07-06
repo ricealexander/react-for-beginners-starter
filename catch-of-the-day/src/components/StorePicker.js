48591243a92c1d50813677ed5be65213f1e0da0a
ricealexander <webdev@ricealexander.com>
2019-07-06 09:19:06 -0500

    :one::two: finish goToStore method for StorePicker component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -8,8 +8,9 @@ class StorePicker extends Component {
     // 1. Stop the form from submitting
     event.preventDefault();
     // 2. get the text from that input
-    console.log(this);
+    const storeName = this.myInput.current.value;
     // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
   }
 
   render() {
@@ -18,7 +19,7 @@ class StorePicker extends Component {
         <h2>Please Enter A Store</h2>
         <input
           type="text"
-          ref="this.myInput"
+          ref={this.myInput}
           placeholder="Store Name"
           defaultValue={getFunName()}
           required
