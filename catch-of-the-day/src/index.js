76c42d8d775a54c35468b54739aa17f5c59fb069
ricealexander <webdev@ricealexander.com>
2019-07-05 20:27:20 -0500

    :fire: remove unused content from index.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-import StorePicker from './components/StorePicker';
+// import StorePicker from './components/StorePicker';
 import App from './components/App';
 
 import "./css/style.css";
