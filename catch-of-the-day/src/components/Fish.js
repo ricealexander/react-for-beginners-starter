4d1623c5249493fd79c600f2dde574bb4e5d514a
ricealexander <webdev@ricealexander.com>
2019-07-06 10:55:50 -0500

    :one::six: create AddToOrder function and hook it up to the fish component

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  }
+
   render() {
     const { image, name, price, desc, status } = this.props.details;
     const isAvailable = (status === 'available');
@@ -13,7 +17,9 @@ class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button disabled={!isAvailable}>{ isAvailable ? 'Add To Order' : 'Sold Out' }</button>
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? 'Add To Order' : 'Sold Out'}
+        </button>
       </li>
     );
   }
