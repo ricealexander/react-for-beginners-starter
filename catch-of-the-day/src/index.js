1fc536d98f8dd76e300b7c85d449b28a80c06b39
ricealexander <webdev@ricealexander.com>
2019-07-05 19:23:59 -0500

    mount react content into #main

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,10 @@
-// let's go!
-alert('hey');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends Component {
+  render() {
+    return <p>Hello!</p>
+  }
+}
+
+render(<p>HEYYYYY</p>, document.querySelector('#main'));
