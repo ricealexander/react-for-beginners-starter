8d1dabd86b112364a45514819a28cbf1f1a507db
ricealexander <webdev@ricealexander.com>
2019-07-06 08:52:26 -0500

    :one::one: apply lessons from button example to goToStore method

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  goToStore(event) {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from that input
+    // 3. Change the page to /store/whatever-they-entered
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input
           type="text"
