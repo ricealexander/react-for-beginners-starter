c99e7fac00a859af9c2b6946825c2163c95ab0f4
ricealexander <webdev@ricealexander.com>
2019-07-28 17:51:57 -0500

    :one::seven: Create a tally of the cost of the order

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends Component {
   render() {
+    const {fishes, order} = this.props;
+    const orderIDs = Object.keys(order);
+
+    const total = orderIDs.reduce((tally, key) => {
+      const fish = fishes[key];
+      const count = order[key];
+      const isAvailable = (fish && fish.status === 'available');
+
+      if (!isAvailable) return tally;
+      return tally + (count * fish.price);
+    }, 0);
+
     return (
       <div className="order-wrap">
         <h2>Order</h2>
-        <ul>
-          <li></li>
-        </ul>
+        {orderIDs}
+        <div className="total">
+          <strong>{formatPrice(total)}</strong>
+        </div>
       </div>
     );
   }
